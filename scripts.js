e3759a9e7c2c90bf422898026ff057db2de17834
Lisa Folkerson <lisafolkerson@gmail.com>
2015-05-13 12:36:10 -0400

    lots of new js

--- scripts.js
+++ scripts.js
@@ -2,19 +2,61 @@
 app = {};
 
 //mke the canvas and the 2d context
-var canvas = document.createElement('canvas');
+var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 // set canvas width and height to be that of the window
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight;x
 
-canvas.id = "theZone";
+// grab the video element with jQuery
+app.video = $('video.webcam')[0];
+// grab the canvas with jquery
+app.canvas = $('canvas.photo')[0];
 
-//add the canvas to the html body
-document.getElementById('theCanvas').appendChild(canvas);
+app.getVideo = function() {
+
+  // normalize so it works on all browsers in the future all browsers will get navigator.getUserMedia()
+  navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+  navigator.getUserMedia ({ video: true,  audio: true }, function(localMediaStream) {
+    // set the video source
+    app.video.src = window.URL.createObjectURL(localMediaStream);
+    // play the video!
+    app.video.play();
+  },
+  // errorCallback
+  function(err) {
+    console.log("The following error occured: " + err);
+ });
+}
+
+app.takePhoto = function() {
+  /* The next four lines set the canvas to the same size as the video */
+  var width = $(app.video).width();
+  var height = $(app.video).height();
+  app.canvas.width = width;
+  app.canvas.height = height;
+
+  // Then we "draw" the video (one frame) to the canvas
+  app.canvas.getContext('2d').drawImage(app.video, 0, 0, width, height);
+  
+  // Then we convert that canvas to a "data blob"  which is like an image src
+  var data = app.canvas.toDataURL('image/png');
+  
+  // 1. create an image 2. set the source the be the data 3. append to an element
+  $('<img>').attr('src',data).appendTo('.strip');
+}
+
+
 
 app.url = 'apius.faceplusplus.com ';
 app.key = 'b76b9735bd2795ac44068c6b4d01d96e';
 app.secret = 'CxYS3FuIjXau6bUckY-KKxaKNGTXOPGw';
 
+
+
+// DOCUMENT READY
+$(function() {
+  app.getVideo();
+})
