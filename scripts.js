9fb484efe2fabb57a9af67d4af16aab000e9b5b8
Lisa Folkerson <lisafolkerson@gmail.com>
2016-05-13 15:07:07 -0400

    can you take a look and see that i've resolved what you've done properly?

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 // define namespace
 app = {};
 
+<<<<<<< Updated upstream
 app.locals = {
 	token : '204078c03d235e0',
 	imgurImg : '',
 }
 
 //make the canvas and the 2d context
+=======
+//mke the canvas and the 2d context
+>>>>>>> Stashed changes
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -24,7 +28,7 @@ app.canvas = $('canvas.photo')[0];
 app.getVideo = function() {
 
   // normalize so it works on all browsers in the future all browsers will get navigator.getUserMedia()
-  navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  navigator.getUserKBMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 
   navigator.getUserMedia ({ video: true,  audio: false }, function(localMediaStream) {
     // set the video source
@@ -61,6 +65,7 @@ app.takePhoto = function() {
 
 }
 
+<<<<<<< Updated upstream
 app.upload = function(img) {
 	var $img = $(img);
 	localStorage.doUpload = true;
@@ -113,6 +118,23 @@ app.init = function(link){
 	detectImageUrl = server + '?url=' + imageUrl + '&api_secret=' + secret + '&api_key=' + key + '&attribute=' + attribute;
 	//concatinates on to our endpoints
 	app.detectFace(detectImageUrl);
+// =======
+// app.init = function(){
+//   $('.imgUrl').on('submit',function(e){
+//     e.preventDefault();
+//     var imageUrl = $('.url').val();
+//     // gets the image url on submit.  later on we will just use this to grab urls.
+//     var server = 'http://api.us.faceplusplus.com/v2/detection/detect';
+//     var key = 'b76b9735bd2795ac44068c6b4d01d96e';
+//     var secret = 'CxYS3FuIjXau6bUckY-KKxaKNGTXOPGw';
+//     var attribute = 'smiling';
+
+//     detectImageUrl = server + '?url=' + imageUrl + '&api_secret=' + secret + '&api_key=' + key + '&attribute=' + attribute;
+//     //concatinates on to our endpoints
+//     app.detectFace(detectImageUrl);
+//     console.log(detectImageUrl);
+//   });
+// >>>>>>> Stashed changes
 }  //init ends here
 
 app.detectFace = function(){
@@ -209,6 +231,7 @@ app.detectSmile = function(){
 
 // DOCUMENT READY
 $(function() {
+<<<<<<< Updated upstream
  	app.getVideo();
 	$('a.snap').on('click',function(e){
 		$(".messageBox").html("<p>Let Me See how You're feeling...</p>");
@@ -229,6 +252,50 @@ $(function() {
 		}, 1000);
 	})
 
+=======
+ //  app.getVideo();
+	// app.init();
+ //  app.detectSmile();
+
+ //  $('a.snap').on('click',function(e) {
+ //      e.preventDefault();
+ //      // find the wait time
+ //      var waitTime = $(this).data('wait');
+ //      var waitedTime = 0;
+
+ //      // if there is no wait, then just take it
+ //      if(!waitTime) {
+ //        app.takePhoto();
+ //        return; // stop the rest from running          
+ //      }
+
+ //      // set a timeout to take the photo after X seconds
+ //      setTimeout(function(){
+ //        app.takePhoto();
+ //      },waitTime);
+
+ //      $('.countdown').text(waitTime / 1000).show();
+
+ //      var interval = setInterval(function(){
+ //        waitedTime+= 1000;
+ //        var timeLeft = (waitTime - waitedTime) / 1000;
+ //        $('.countdown').text(timeLeft);
+ //        if(waitedTime >= waitTime) {
+ //          clearInterval(interval);
+ //          $('.countdown').hide();
+ //        }
+ //      },1000);
+
+
+ //    });
+
+ //    $('a.flip').on('click',function(e) {
+ //      e.preventDefault();
+ //      $(app.video).toggleClass('flipped');
+ //    });
+
+  
+>>>>>>> Stashed changes
 })
 
 
